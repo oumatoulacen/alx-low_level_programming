b00a73c67eee30ffdfc8f0102c0864df4773b74c
oumatoulacen <oumatoulacen@gmail.com>
2023-03-16 15:38:41 +0100

    d

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
-#include<stdio.h>
-
+#include <stdio.h>
 int main(void)
 {
-``` here we use puts instead printf```
-puts("\"Programming is like building a multilingual puzzle");
+/**
+ * puts instead of printf
+ */
+puts ("\"Programming is like building a multilingual puzzle");
 return (0);
 }
