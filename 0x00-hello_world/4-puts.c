b20959b14ea825862bc01cf7eb0172d94dcb1e18
oumatoulacen <oumatoulacen@gmail.com>
2023-03-16 16:00:33 +0100

    d

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-/**
- * puts instead of printf
- */
 int main(void)
 {
+	/**
+ * puts instead of printf
+ */
+
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-/**
- * puts instead of printf
- */
-
 int main(void)
 {
+	/**
+ * printf not puts
+ */
+
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
