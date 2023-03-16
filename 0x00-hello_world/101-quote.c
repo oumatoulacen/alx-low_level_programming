a26c48c7805e1a1a1dde9cbcd65d13e40704a533
oumatoulacen <oumatoulacen@gmail.com>
2023-03-16 20:48:27 +0100

    d

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,10 @@
  *
  * Return: Always 0 (Success)
  */
-int main() {
-    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, message, 50);
-    return 1;
+int main(void)
+{
+char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, message, 50);
+return 1;
 }
 
