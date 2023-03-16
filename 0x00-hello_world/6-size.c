786392a3931bb925cf31c0c2a228282b0362e853
oumatoulacen <oumatoulacen@gmail.com>
2023-03-16 16:30:14 +0100

    d

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,10 +3,10 @@
 
 int main(void)
 {
-printf("Size of a char: %c \n
-Size of an int: %d \n
-Size of a long int: %ld \n
-Size of a long long int: %lld \n
-Size of a float: %f \n",sizeof(char),sizeof(int),(long) sizeof(int),(long long) sizeof(int),sizeof(float));
-return (0);
+	printf("Size of a char: %c\n", sizeof(char));
+	printf("Size of an int: %d\n", sizeof(int));
+	printf("Size of a long int: %dl\n", sizeof(long int));
+	printf("Size of a long long int: %dll\n", sizeof(long long int));
+	printf("Size of a long float: %f\n", sizeof(float));
+	return (0);
 }
