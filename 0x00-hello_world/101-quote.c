fe8564155121d09cf6af3893f1c7f72cf0c76d16
oumatoulacen <oumatoulacen@gmail.com>
2023-03-16 20:44:51 +0100

    d

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-write("and that piece of art is useful\" - Dora Korpar, 2015-10-19")
-return (1);
+int main() {
+    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, message, 50);
+    return 1;
 }
+
