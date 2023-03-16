600a21e3969bc1e2c2f996cc9f576fa49f264ab2
oumatoulacen <oumatoulacen@gmail.com>
2023-03-16 15:05:20 +0100

    d

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
 
 
-/*print using puts only */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
