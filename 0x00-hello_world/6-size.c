cc5ebaf06285080e3dd2999d5a86da408e7b4919
oumatoulacen <oumatoulacen@gmail.com>
2023-03-16 16:16:00 +0100

    d

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+
+int main(void)
+{
+printf("Size of a char: %c \n
+Size of an int: %d \n
+Size of a long int: %ld \n
+Size of a long long int: %lld \n
+Size of a float: %f \n",sizeof(char),sizeof(int),(long) sizeof(int),(long long) sizeof(int),sizeof(float));
+return (0);
+}
