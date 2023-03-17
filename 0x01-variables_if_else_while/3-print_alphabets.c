48e031b1fc96136e3556effeed09b12635146495
oumatoulacen <oumatoulacen@gmail.com>
2023-03-17 16:09:22 +0100

    e

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 success
+ */
+
+int main(void)
+{
+	int l = 'a';
+
+	while (l <= "Z")
+	{
+		printf(l);
+	return (0);
+	}
+}
