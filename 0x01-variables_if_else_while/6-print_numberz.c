3f61c0e654a43995ea8eca70df7f53b61462f686
oumatoulacen <oumatoulacen@gmail.com>
2023-03-17 17:47:29 +0100

    e

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 success
+ */
+
+int main(void)
+{
+	int a;
+
+	for (i = 0; i <= 9; i++)
+	{
+	putchar('a');
+	}
+	return (0);
+}
+
