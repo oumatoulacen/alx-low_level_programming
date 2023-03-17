773dd40203f9f8577685904b3ce7efc62c2f8f7b
oumatoulacen <oumatoulacen@gmail.com>
2023-03-17 16:56:09 +0100

    e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - Entry Point
+ *
+ * Return: Always 0 success
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'a')
+	{
+	if (c != 'q' || c != 'e')
+	{
+	putchar(c);
+	}
+	}
+
+	return (0);
+}
