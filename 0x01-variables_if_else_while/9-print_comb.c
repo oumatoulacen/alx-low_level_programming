3075847aa63c050b540f317bfc4181af204c19f2
oumatoulacen <oumatoulacen@gmail.com>
2023-03-17 18:57:44 +0100

    e

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 success
+ */
+
+int main(void)
+{
+	int d = 0;
+
+	while (d <= 9)
+	{
+	putchar(d + '0');
+	d++;
+	putchar(','};
+	putchar(' ');
+	}
+	return (0);
+}
