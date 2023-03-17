d813b1b5632c726eb1be17f805f6d0eec7dc8cf1
oumatoulacen <oumatoulacen@gmail.com>
2023-03-17 16:40:53 +0100

    e

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,15 +8,18 @@
 
 int main(void)
 {
-	char l = 'A';
+	char l = 'a';
+	char L = 'A';
 
-	while (l <= 'Z')
+	while (l <= 'z')
 	{
-	char L = l + 26;
-
-	putchar(L);
 	putchar(l);
 	l++;
+	}
+	while (L <= 'Z')
+	{
+	putchar(L);
+	L++;
 	}
 	putchar('\n');
 	return (0);
