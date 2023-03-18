5836ea71c73023eecd0fcb26c46ccf06189bbe77
oumatoulacen <oumatoulacen@gmail.com>
2023-03-18 01:20:46 +0100

    e

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 success
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i; j < 100; j++)
+		{
+			if (i == j)
+				continue;
+			if (i < 10)
+			{
+				putchar('0' + i / 10);
+				putchar('0' + i % 10);
+			}
+			else
+			{
+				putchar('0' + i / 10);
+				putchar('0' + i % 10);
+				putchar(' ');
+				if (j < 10)
+				{
+					putchar('0' + j / 10);
+					putchar('0' + j % 10);
+				}
+				else
+				{
+					putchar('0' + j / 10);
+					putchar('0' + j % 10);
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		return (0);
+}
