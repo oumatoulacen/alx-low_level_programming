8e1f0b50cd7e5d108b0221bba9413281e2d8c8f0
oumatoulacen <oumatoulacen@gmail.com>
2023-03-21 21:56:51 +0000

    h

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ *jack_bauer - prints every minute of the day
+ *
+ * Return: Always 0.
+ */
+
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 23; i++)
+	{
+		for (j = 0; j <= 59; j++)
+		{
+			if (i <= 9)
+			{
+				_putchar(48);
+				_putchar(i + 48);
+				_putchar(58);
+					if (j < 9)
+					{
+					_putchar(48);
+					_putchar(j + 48);
+					}
+					else
+					{
+						_putchar(j + 48);
+					}
+			}
+			else
+			{
+				_putchar(i + 48);
+				_putchar(58);
+				if (j < 9)
+				{
+					_putchar(48);
+					_putchar(j + 48);
+				}
+				else
+				{
+					_putchar(j + 48);
+				}
+			}
+		}
+	}
+}
