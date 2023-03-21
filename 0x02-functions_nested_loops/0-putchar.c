1bc14a5d4af5b532d2c29c44f09638eab8465418
oumatoulacen <oumatoulacen@gmail.com>
2023-03-21 13:17:13 +0000

    h

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "header.h" 
+
+
+/**
+ * main - entry function
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+}
