29099fcd0d7349a835825040a2c1295bb240823c
oumatoulacen <oumatoulacen@gmail.com>
2023-03-21 20:57:09 +0000

    h

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,7 +7,9 @@
  */
 int _isalpha(int c)
 {
-	if (c <= 122 && c >= 97 || c <= 90 && c >= 65)
+	if (c <= 122 && c >= 97)
+		return (1);
+	else if (c <= 90 && c >= 65)
 		return (1);
 	else
 		return (0);
