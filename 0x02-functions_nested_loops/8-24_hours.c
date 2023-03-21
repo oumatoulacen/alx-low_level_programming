d1f85bca0f353dd99e67fd23edbba33aac75f092
oumatoulacen <oumatoulacen@gmail.com>
2023-03-21 22:07:02 +0000

    h

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,37 +13,29 @@ void jack_bauer(void)
 
 	for (i = 0; i <= 23; i++)
 	{
+
 		for (j = 0; j <= 59; j++)
 		{
+
 			if (i <= 9)
 			{
 				_putchar(48);
 				_putchar(i + 48);
-				_putchar(58);
-					if (j < 9)
-					{
-					_putchar(48);
-					_putchar(j + 48);
-					}
-					else
-					{
-						_putchar(j + 48);
-					}
 			}
 			else
 			{
 				_putchar(i + 48);
-				_putchar(58);
-				if (j < 9)
-				{
-					_putchar(48);
-					_putchar(j + 48);
-				}
-				else
-				{
-					_putchar(j + 48);
-				}
 			}
+			_putchar(58);
+			if (j < 9)
+			{
+				_putchar(48);
+				_putchar(j + 48);
+			}
+			else
+				{
+					_putchar(j + 48);
+				}
 		}
-	}
+		_putchar('\n');
 }
