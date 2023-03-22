fcde69886d2c5cadd9d6f2168d6b04c184d09d0e
oumatoulacen <oumatoulacen@gmail.com>
2023-03-22 11:50:13 +0000

    h

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add  - add a and b
+ *@a: first num
+ *@b: second num
+ * Return: Always 0.
+ */
+
+int add(int a, int b)
+{
+	int a;
+	int b;
+
+	result = a + b;
+	return (result);
+}
