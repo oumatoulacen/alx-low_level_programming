3a22a5dd8fd12d41b7cff07fca6a9696fe1152b8
oumatoulacen <oumatoulacen@gmail.com>
2023-03-22 14:14:02 +0000

    h

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,11 +12,12 @@ void print_to_98(int n)
 	{
 		int i;
 
-		for (i = n; i >= 98; i--)
+		for (i = n; i > 98 ; i--)
 		{
 			if (i >= 100)
 			{
-				_putchar(i / 100);
+
+				_putchar(i / 100 + '0');
 				_putchar(((i % 100) / 10) + '0');
 				_putchar(((i % 100) % 10) + '0');
 			}
@@ -25,17 +26,80 @@ void print_to_98(int n)
 				_putchar(((i % 100) / 10) + '0');
 				_putchar(((i % 100) % 10) + '0');
 			}
+			_putchar(',');
+			_putchar(' ');
 		}
 	}
-	else if (n <= 98)
+	else if (n < 98 && n >= 0)
 	{
 		int j;
+		int k;
 
-		for (j = n; j >= 98; j++)
+		for (j = n / 10; j >= 9; j++)
 		{
-			_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');
+			for (k = n % 10; k >= 8; k++)
+			{
+				if (j == 0)
+				{
+					_putchar('0');
+					_putchar(',');
+					_putchar(' ');
+				else if (j < 10)
+				{	
+					_putchar(k + '0');
+					_putchar(',');
+					_putchar(' ');
+				}
+				}else
+				{
+				_putchar(j + '0');
+				_putchar(k + '0');
+				_putchar(',');
+				_putchar(' ');
+				}
+			}
 		}
 	}
+	
+	else if (n < 0)
+		{
+			int l;
+			int g;
 
+			for (l = n / 10; l >= 9; l++)
+			{
+				for (g = n % 10; g >= 8; g++)
+				{
+					if (l == 0)
+					{
+						_putchar('-');
+						_putchar(-g + '0');
+						_putchar(',');
+						_putchar(' ');
+					}
+					else if (l >= -9)
+					{
+						_putchar('-');
+						_putchar(-l + '0');
+						_putchar(-g + '0');
+						_putchar(',');
+						_putchar(' ');
+					}
+					else
+					{
+						_putchar('-');
+						_putchar((-l / 10) + '0');
+						_putchar(-l + '0');
+						_putchar(-g + '0');
+						_putchar(',');
+						_putchar(' ');
+					}
+
+		}
+	else if (n == 98)
+	{
+		_putchar(9 + '0');
+		-putchar(8 + '0');
+		_putchar(',');
+		_putchar(' ');
 }
