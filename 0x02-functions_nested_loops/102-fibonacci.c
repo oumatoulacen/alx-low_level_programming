3af87e4a89bf141686fc670c73b841a8a28991c7
oumatoulacen <oumatoulacen@gmail.com>
2023-03-23 02:08:28 +0000

    h

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,10 +1,7 @@
-102-fibonacci.c
-
 #include <stdio.h>
 
 /**
- * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
- *        separated by a comma followed by a space.
+ * main - Prints first 50 Fibonacci numbers
  *
  * Return: Always 0.
  */
