ee7198a789d5fd02c5602cceb00c232ab74c3667
oumatoulacen <oumatoulacen@gmail.com>
2023-03-23 20:09:07 +0000

    k

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ *@size: the size of the triangle
+ * Return: Always 0.
+ */
+
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (k = size - i -1; k > 0; k--)
+			{
+				_putchar(' ');
+			}
+			for (j = 0; j < i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
