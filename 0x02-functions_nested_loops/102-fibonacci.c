17c6a9c2665ee20e02fe5410d62707c1de64c163
oumatoulacen <oumatoulacen@gmail.com>
2023-03-23 01:57:23 +0000

    h

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,7 +11,7 @@ int main(void)
 {
 
 	int a = 1, b = 2;
-	long int fib[50] = {0};
+	unsigned long fib[50] = {0};
 	int i;
 	int j;
 
@@ -26,11 +26,11 @@ int main(void)
 		a = b;
 		b = c;
 	}
-	printf("%li", fib[0]);
+	printf("%lu", fib[0]);
 
 	for (j = 1; j < 50; j++)
 	{
-		printf(", %li", fib[j]);
+		printf(", %lu", fib[j]);
 	}
 	printf("\n");
 	return (0);
