e3203f9cfdebfb910b3bcdc88d188d8e193d742b
oumatoulacen <oumatoulacen@gmail.com>
2023-03-23 15:29:36 +0000

    k

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -9,19 +9,7 @@
 int _isupper(int c)
 {
 	if (c <= 90 && c >= 65)
-	{
-		_putchar(c);
-		_putchar(':');
-		_putchar(' ');
-		_putchar(49);
 		return (1);
-	}
-	else if (c <= 122 && c >= 97)
-	{
-		_putchar(c);
-		_putchar(':');
-		_putchar(' ');
-		_putchar(48);
-		return (0);
-	}
+
+	return (0);
 }
