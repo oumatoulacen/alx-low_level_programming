37de3710e78f4fd19a58e94194385150336646db
oumatoulacen <oumatoulacen@gmail.com>
2023-03-29 17:17:06 +0000

    ptr

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,12 +15,12 @@ int _strcmp(char *s1, char *s2)
 	int jres = 0;
 	int ires = 0;
 
-	while (s1[i] != '\0')
+	while (s1[i])
 	{
 		ires = ires + s1[i];
 		i++;
 	}
-	while (s2[j] != '\0')
+	while (s2[j])
 	{
 		jres += s2[j];
 		j++;
