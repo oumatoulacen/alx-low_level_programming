22903b4193a8f7bbbb7d7ae83f400132ec131289
oumatoulacen <oumatoulacen@gmail.com>
2023-03-29 17:52:30 +0000

    ptr

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,23 +9,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int res;
 	int i = 0;
-	int j = 0;
-	int jres = 0;
-	int ires = 0;
 
-	while (s1[i])
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		ires = ires + s1[i];
+
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 		i++;
 	}
-	while (s2[j])
-	{
-		jres += s2[j];
-		j++;
-	}
-	res = ires - jres;
-	return (res);
+	return (0);
 }
 
