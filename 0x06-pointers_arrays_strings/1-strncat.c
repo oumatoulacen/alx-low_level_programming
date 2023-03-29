a2ee7e9c1ecd99f1a36b6aabefef4f53d9e73864
oumatoulacen <oumatoulacen@gmail.com>
2023-03-29 15:58:32 +0000

    ptr

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings.
+ * @dest: dst
+ * @src: src
+ * Return: pointer to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *pt = dest;
+	int i = 0;
+
+	while (*pt != '\0')
+	{
+		pt++;
+	}
+	while (*src != '\0' && i < n)
+	{
+		*pt++ = *src++;
+		i++;
+	}
+	*pt++ = '\0';
+	return (dest);
+}
