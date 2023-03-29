10b30937cba6bbd4c66ee1c85c35ff6ce08556e0
oumatoulacen <oumatoulacen@gmail.com>
2023-03-29 15:48:13 +0000

    ptr

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,17 +9,17 @@
 
 char *_strcat(char *dest, char *src)
 {
-    char* pt = dest;
+	char *pt = dest;
 
-    while (*pt != '\0')
-    {
-        pt++;
-    }
-    while (*src != '\0')
-    { 
-        *pt++ = *src++;
-    }
-    *pt++ = '\0';
-    return dest; 
+	while (*pt != '\0')
+	{
+		pt++;
+	}
+	while (*src != '\0')
+	{
+		*pt++ = *src++;
+	}
+	*pt++ = '\0';
+	return (dest);
 }
 
