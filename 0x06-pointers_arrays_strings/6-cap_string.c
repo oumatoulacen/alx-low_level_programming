5a76964ec6d7ebe310d5542640593cd9682cfebb
oumatoulacen <oumatoulacen@gmail.com>
2023-03-30 01:46:39 +0000

    ptr

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -19,6 +19,10 @@ char *cap_string(char *str)
 		{
 			if (str[i] == sp[j] || str[i] == '\n' || str[i] == '\t')
 			{
+				if (str[i] == '\t')
+				{
+					str[i] == ' '
+				}
 				if (str[i + 1] <= 122 && str[i + 1] >= 97)
 				{
 				str[i + 1] = str[i + 1] - 32;
