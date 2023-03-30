fb98367e7af0db2391664f3cba0783c34b6ec1e6
oumatoulacen <oumatoulacen@gmail.com>
2023-03-30 17:19:57 +0000

    ptr

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,18 +8,23 @@
 
 char *rot13(char *str)
 {
-	char *p = str;
+	int j;
+	char f1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char f2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char *ptr = str;
 
-	while (*p)
+	while (*str)
 	{
-		if ((*p >= 'a' && *p <= 'm') || (*p >= 'A' && *p <= 'M'))
+		for (j = 0; j <= 52; j++)
 		{
-			*p += 13;
+		if (*str == f1[j])
+		{
+			*str = f2[j];
+			break;
 		}
-		else if ((*p >= 'n' && *p <= 'z') || (*p >= 'N' && *p <= 'Z'))
-			*p -= 13;
-		p++;
+		}
+	str++;
 	}
-	return (str);
+	return (ptr);
 }
 
