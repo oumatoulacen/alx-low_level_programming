4be2497f79af3f6c2438d194814622ed5c709984
oumatoulacen <oumatoulacen@gmail.com>
2023-03-30 02:25:48 +0000

    ptr

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -17,13 +17,13 @@ char *cap_string(char *str)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			if (str[i] == sp[j] || str[i] == '\n' || str[i] == '\t')
+			if (str[i] == sp[j] || str[i] == '"'|| str[i] == '\n' || str[i] == '\t')
 			{
+				if (str[i] == '\t')
+					str[i] = ' ';
 				if (str[i + 1] <= 122 && str[i + 1] >= 97)
 				{
-					if (str[i] == '\t')
-						str[i] = ' ';
-				str[i + 1] = str[i + 1] - 32;
+					str[i + 1] = str[i + 1] - 32;
 				}
 			}
 		}
