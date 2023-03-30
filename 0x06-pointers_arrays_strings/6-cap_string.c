ceb9415e9c32d96d1550065907a77b6457f3d726
oumatoulacen <oumatoulacen@gmail.com>
2023-03-30 01:43:22 +0000

    ptr

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string.
+ * @str: string
+ * Return: ptr
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+	int j;
+	char *sp = ", .?!(){};";
+
+
+	while (str[i])
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (str[i] == sp[j] || str[i] == '\n' || str[i] == '\t')
+			{
+				if (str[i + 1] <= 122 && str[i + 1] >= 97)
+				{
+				str[i + 1] = str[i + 1] - 32;
+				}
+
+			}
+		}
+		i++;
+	}
+	return (str);
+}
