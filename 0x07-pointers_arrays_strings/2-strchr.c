62595c58e301201fde3c3376961c62def2fdefd4
oumatoulacen <oumatoulacen@gmail.com>
2023-04-03 15:19:42 +0000

    ptr-arr-str

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,11 +9,13 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	int i = 0;
+
+	while (s[i] != '\0')
 	{
-		if (*s == c)
-			return (s);
-		s++;
+		if (s[i] == c)
+			return (&s[i]);
+		i++;
 	}
 	return (0);
 }
