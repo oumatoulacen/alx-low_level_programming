4c019f76b261989daefd617be6fbe361d37fbc6c
oumatoulacen <oumatoulacen@gmail.com>
2023-04-03 14:03:47 +0000

    ptr-arr-str

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string.
+ * @s: string
+ * @c: char
+ * Return: ptr
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (&s[i]);
+		i++;
+	}
+	return (s);
+}
