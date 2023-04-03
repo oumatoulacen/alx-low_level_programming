eb537c2d0d910c6be281f76d1b8904adbf8a25f1
oumatoulacen <oumatoulacen@gmail.com>
2023-04-03 15:59:58 +0000

    ptr-arr-str

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,5 +3,23 @@
 /**
  * _strspn - gets the length of a prefix substring
  * @s: string
- * @accept: 
+ * @accept: var
+ * Return: unsigned int
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	unsigned int k = 0;
+
+	for (i = 0; s[i] != ' '; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+					k++;
+		}
+	}
+	return (k);
+}
 
