0a7b42bdb565dfe7b8869401b3c558c99b94dc46
oumatoulacen <oumatoulacen@gmail.com>
2023-04-05 16:12:59 +0000

    recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,23 +3,30 @@
 /**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: num
+ * @i: iter
  * Return: square root of n
  */
+int find_square(int n, int i);
 
 int _sqrt_recursion(int n)
 {
-	int res = 1;
-	int num = 1;
+	if (n < 0)
+		return (-1);
+	return (find_square(n, 0));
+}
 
-	if (n == 0 || n == 1)
-		return (n);
-	while (res <= n)
-	{
-		res = num * num;
-		if (res == n && num <= n)
-			return (num);
-		num++;
-	}
-	return (-1);
+/**
+ * find_square - find the square root
+ * @i: iter
+ * @n: num
+ * Return: square root
+ */
 
+int find_square(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (find_square(n, i + 1));
 }
