5cfc15edc58d73f6657cadf4e91151dcf04b82f7
oumatoulacen <oumatoulacen@gmail.com>
2023-04-07 12:04:09 +0000

    static libraries

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -1,1 +1,21 @@
-char *_memcpy(char *dest, char *src, unsigned int n){};
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: dst
+ * @src: src
+ * @n: num
+ * Return: ptr
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *ptr = dest;
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*dest++ = *src++;
+	}
+	return (ptr);
+}
--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,1 +1,16 @@
-int _abs(int n){};
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer.
+ *@d: the integer
+ * Return: Always 0.
+ */
+int _abs(int d)
+{
+	if (d < 0)
+		return (-d);
+	if (d > 0)
+		return (d);
+	else
+		return (0);
+}
