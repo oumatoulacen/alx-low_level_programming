7890e0ad96eda93c285aa4eae0e9ed951a13018d
oumatoulacen <oumatoulacen@gmail.com>
2023-04-07 18:25:48 +0000

    args

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ * main - check the code
+ * @argc: number of args
+ * @argv: array of size argc
+ * Return: always 0
+ */
+
+int main(__attribute__((unused)) int argc, char **argv)
+{
+	int i, res = 0;
+
+	for (i = 0; i < argc; i++)
+	{
+		if (atoi(argv[i]) >= 0)
+		{
+			res += atoi(argv[i]);
+		}
+
+	}
+	printf("%d\n", res);
+	return (0);
+}
