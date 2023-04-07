4e6bf5c1ded9264084a384ccb20d94ee886dfa18
oumatoulacen <oumatoulacen@gmail.com>
2023-04-07 15:51:57 +0000

    static libraries

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string
+ * @accept: var
+ * Return: unsigned int
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	unsigned int k = 0;
+
+	for (i = 0; s[i] != ' '; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				k++;
+		}
+	}
+	return (k);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,3 @@
+static libraries
+[0. A library is not a luxury but one of the necessities of life](libmy.a)
+[1. Without libraries what have we? We have no past and no future](create_static_lib.sh)
