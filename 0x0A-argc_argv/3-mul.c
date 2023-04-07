62ae68fc394d621bcdcd05771802bcd990f5a94b
oumatoulacen <oumatoulacen@gmail.com>
2023-04-07 18:15:49 +0000

    args

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ * main - check the code
+ * @argc: number of args
+ * @argv: array of size argc
+ * Return: always 0
+ */
+
+int main(__attribute__((unused)) int argc, char **argv)
+{
+	if (argc == 3)
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	else
+		printf("Error");
+	return (0);
+}
