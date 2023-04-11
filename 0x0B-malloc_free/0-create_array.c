1bdf32777d29e6920366266ea0174dc091872fbd
oumatoulacen <oumatoulacen@gmail.com>
2023-04-11 12:39:11 +0000

    mlc fr

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,19 +1,22 @@
 #include "main.h"
 #include <stdlib.h>
+#include <limits.h>
+
 
 /**
  * create_array - create an array of chars
  * @size: size of arr
  * @c: char to fill arr with
+ * Return: arr
  */
 
 
 char *create_array(unsigned int size, char c)
 {
 	char *arr = malloc(sizeof(char) * size);
-	long unsigned int i = 0;
+	unsigned long int i = 0;
 
-	if (arr)
+	if (arr != NULL)
 	{
 		while (i < sizeof(char) * size)
 		{
