67f4d2faebe01364d20bdbb0532c2f49a54119ca
oumatoulacen <oumatoulacen@gmail.com>
2023-04-11 16:02:57 +0000

    mlc fr

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,6 +15,10 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0, k, l;
 	char *ptr;
 
+	if (s1 == NULL)
+		s1 = '\0';
+	if (s2 == NULL)
+		s2 = '\0';
 	while (s1[i])
 		i++;
 	while (s2[j])
@@ -23,19 +27,9 @@ char *str_concat(char *s1, char *s2)
 	if (ptr != NULL)
 	{
 		for (k = 0; k < i; k++)
-		{
-			if (s1 == NULL)
-				ptr = '\0';
-			else
-				ptr[k] = s1[k];
-		}
+			ptr[k] = s1[k];
 		for (l = 0; l <= j; l++)
-		{
-			if (s2 == NULL)
-				ptr[k + l] = '\0';
-			else
-				ptr[k + l] = s2[l];
-		}
+			ptr[k + l] = s2[l];
 		return (ptr);
 	}
 	return (0);
