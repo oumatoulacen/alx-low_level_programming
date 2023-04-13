0804d1fd8320e88f35493efccfc4d04f7900fb91
oumatoulacen <oumatoulacen@gmail.com>
2023-04-13 23:29:41 +0000

    more malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -19,7 +19,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i;
 	size_t copy_size;
 
-	if (new_size == 0)
+	if (new_size == 0 && ptr == NULL)
 	{
 		free(ptr);
 		return (0);
