372ad701cef69e4f9c11699e6a57c2863102103c
oumatoulacen <oumatoulacen@gmail.com>
2023-04-13 23:34:23 +0000

    more malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -21,7 +21,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (ptr == NULL)
 		return (malloc(new_size));
-	if (new_size == 0 && ptr == NULL)
+	if (new_size == 0)
 	{
 		free(ptr);
 		return (0);
