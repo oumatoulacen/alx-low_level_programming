a42d55dc1ff8df54df748a28ac467da79b808d7b
oumatoulacen <oumatoulacen@gmail.com>
2023-04-17 14:24:39 +0000

    struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,7 +11,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	(*d).name = name;
-	(*d).age = age;
-	(*d).owner = owner;
+	struct dog *b;
+	
+	b = d;
+	(*b).name = name;
+	(*b).age = age;
+	(*b).owner = owner;
 }
