88707abddc3a6087fc93c0d6822d57385789d041
oumatoulacen <oumatoulacen@gmail.com>
2023-04-17 16:06:50 +0000

    struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "dog.h"
+
+
+/**
+ * print_dog - print struct dog
+ * @d: struct sog
+ * Return: void
+ */
+
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		putchar(0);
+	else
+	{
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+	if (d->age == 0)
+		printf("(nil)\n");
+	else
+		printf("Name: %f\n", d->age);
+	if (d->owner == NULL)
+		printf("(nil)\n");
+	else
+		printf("Name: %s\n", d->owner);
+	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,6 +16,6 @@ struct dog
 	char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
+void print_dog(struct dog *d);
+
 #endif
