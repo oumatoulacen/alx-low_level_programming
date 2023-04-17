a4c866ad145533ebeadb17d6b9dc01006df8492a
oumatoulacen <oumatoulacen@gmail.com>
2023-04-17 14:19:03 +0000

    struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,14 @@
 #include "dog.h"
 
+/**
+ * init_dog - initialize variable d
+ * @d: var
+ * @name: value of mem1
+ * @age: value of mem2
+ * @owner: value of mem3
+ * Return:void
+ */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->name = name;
