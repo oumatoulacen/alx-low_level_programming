394aa0ea561003e1652b29c09478072e37cfa235
oumatoulacen <oumatoulacen@gmail.com>
2023-04-17 14:04:22 +0000

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,19 @@
 #ifndef DOG_H
 
 #define DOG_H
+/**
+ * struct dog - dog struct
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: Longer description
+ */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
-	char * owner;
+	char *owner;
 }
+#endif
