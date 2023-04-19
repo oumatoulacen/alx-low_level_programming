6b4e79539f239fe89546306b0ef89b46d0755897
oumatoulacen <oumatoulacen@gmail.com>
2023-04-19 15:45:33 +0000

    function ptr

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - searchs for an int
+ * @array: arr
+ * @size: size of arr
+ * @cmp: pointer to a function
+ * Return: int
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	int c = 0;
+
+	if (size <= 0)
+		return (-1);
+	if (cmp != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp[i] != 0)
+				return (i);
+		}
+		return (-1);
+	}
+}
