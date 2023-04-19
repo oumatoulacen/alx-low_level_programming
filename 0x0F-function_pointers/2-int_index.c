f7082b55c03d5d2eebeb12d4e49529f2b28a5280
oumatoulacen <oumatoulacen@gmail.com>
2023-04-19 15:56:05 +0000

    function ptr

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,15 +13,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (cmp ==NULL || array == NULL ||size <= 0)
 		return (-1);
-	if (cmp != NULL)
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
-		{
-			if (cmp(array[i]) != 0)
-				return (i);
-		}
+		if (cmp(array[i]) != 0)
+			return (i);
 	}
 	return (-1);
 }
