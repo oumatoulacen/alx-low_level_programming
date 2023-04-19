dcc4f875a241311aec6b21151322781b343ff098
oumatoulacen <oumatoulacen@gmail.com>
2023-04-19 23:23:23 +0000

    function ptr

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include "3-calc.h"
 
-
+/**
+ * get_op_func - selects the correct function to perform
+ * the operation asked by the user
+ * @s: operator passed as argument to the program
+ *
+ * Return: pointer to the corresponding function
+ */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 {
 	int num1, num2, result;
 	int (*op_func)(int, int);
-	
+
 	if (argc != 4)
 	{
 		printf("Error\n");
