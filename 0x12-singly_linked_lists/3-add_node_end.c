ae364ee09619b8dccfcbb1afd13fcede3eb8ce94
oumatoulacen <oumatoulacen@gmail.com>
2023-04-27 19:16:09 +0100

    task 4

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -16,24 +16,20 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new;
 	int i = 0;
 
-	while (str[i] != '\0')
-		i++;
-
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (0);
+	while (str[i] != '\0')
+		i++;
 	new->str = strdup(str);
 	new->len = i;
 	new->next = NULL;
 
 	if (*head == NULL)
-		*head = new;
-	else
-	{
-		curr = *head;
-		while (curr->next != NULL)
+		*head = malloc(sizeof(list_t));
+	curr = *head;
+	while (curr != NULL)
 			curr = curr->next;
-	}
 	curr->next = new;
 	return (new);
 }
