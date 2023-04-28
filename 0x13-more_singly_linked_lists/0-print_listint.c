d0b62cfdaadcfe37295aad7a942aa5eb4da6fb2c
oumatoulacen <oumatoulacen@gmail.com>
2023-04-28 11:42:53 +0100

    print the integer of all elements

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list
+ * @h: singly linked list
+ * Return: number of elements in a linked list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int i = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
