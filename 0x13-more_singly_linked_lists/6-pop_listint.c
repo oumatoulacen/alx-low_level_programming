c826ed7ee51fd719578bd189277cffb2ec694f65
oumatoulacen <oumatoulacen@gmail.com>
2023-04-29 15:30:56 +0100

    pop a node from the list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of a listint_t linked list
+ * @head: first
+ * Return: the head node’s data (n).
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *first;
+	int n;
+
+	if (head == NULL)
+		return (0);
+	first = (*head)->next;
+	n = (*head)->n;
+	*head = first;
+
+	return (n);
+}
