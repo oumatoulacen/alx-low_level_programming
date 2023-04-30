4b371f84a6e4a8c22b2a9753635bbd47add8fd37
oumatoulacen <oumatoulacen@gmail.com>
2023-05-01 00:05:24 +0100

    print safe singly linked list out of loop

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints a listint_t linked list.
+ * @head: pointer to head node.
+ * Return: the number of nodes in the list
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t count = 0;
+	const listint_t *current, *next;
+
+	if (head == NULL)
+		exit(98);
+
+	current = head;
+	while (current != NULL)
+	{
+		next = current->next;
+		printf("[%p] %d\n", (void *)current, current->n);
+		count++;
+		if (next >= current)
+		{
+			printf("-> [%p] %d\n", (void *)next, next->n);
+			break;
+		}
+		current = next;
+	}
+	return (count);
+}
