9753b21ebed58d89361facb7e9b0d6f99d8f64a7
oumatoulacen <oumatoulacen@gmail.com>
2023-04-30 18:58:24 +0100

    delet a new node in a given position

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+
+/**
+ * delete_nodeint_at_index - deletes the node at given index .
+ * @head: address of fisrt
+ * @index: at which point to delete the node
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *pre, *temp;
+	unsigned int i;
+
+	if (head == NULL)
+		return (-1);
+	if (*head == NULL && index != 0)
+		return (-1);
+	if (index == 0)
+	{
+		if (*head == NULL)
+			return (-1);
+		pre = *head;
+		*head = (*head)->next;
+		free(pre);
+		return (1);
+	}
+	pre = *head;
+	for (i = 2; i <= index; i++)
+	{
+		pre = pre->next;
+	}
+	temp = pre->next;
+	pre->next = pre->next->next;
+	free(temp);
+	return (1);
+}
