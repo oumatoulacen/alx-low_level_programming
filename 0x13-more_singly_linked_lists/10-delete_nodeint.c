777f3b4afd818cb970ba6dac65816c4c8925b95f
oumatoulacen <oumatoulacen@gmail.com>
2023-04-30 19:00:47 +0100

    delet a new node in a given position

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -18,12 +18,10 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (head == NULL)
 		return (-1);
-	if (*head == NULL && index != 0)
+	if (*head == NULL)
 		return (-1);
 	if (index == 0)
 	{
-		if (*head == NULL)
-			return (-1);
 		pre = *head;
 		*head = (*head)->next;
 		free(pre);
