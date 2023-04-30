2baefb95cbe840c2c64065c55cfaaa5252fbfab1
oumatoulacen <oumatoulacen@gmail.com>
2023-05-01 00:52:11 +0100

    print safe singly linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -4,35 +4,37 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - prints a listint_t linked list safely.
+ * print_listint_safe - prints a listint_t linked list.
  * @head: pointer to head node
  *
- * Return: number of nodes in the list
+ * Return: the number of nodes in the list
  */
-
 size_t print_listint_safe(const listint_t *head)
 {
-    size_t count = 0;
-    const listint_t *tortoise = head, *hare = head;
+	size_t count = 0;
+	const listint_t *current, *next;
+	const listint_t *printed_nodes[1024] = {NULL};
 
-    if (head == NULL)
-        exit(98);
+	if (head == NULL)
+		exit(98);
 
-    while (tortoise != NULL && hare != NULL && hare->next != NULL)
-    {
-        tortoise = tortoise->next;
-        hare = hare->next->next;
-        printf("[%p] %d\n", (void *)tortoise, tortoise->n);
-        count++;
-
-        if (tortoise == hare)
-        {
-            printf("-> [%p] %d\n", (void *)hare, hare->n);
-            printf("Loop starts at [%p]\n", (void *)tortoise);
-            exit(98);
-        }
-    }
-
-    return (count);
+	current = head;
+	while (current != NULL)
+	{
+		printf("[%p] %d\n", (void *)current, current->n);
+		printed_nodes[count] = current;
+		count++;
+		next = current->next;
+		for (i = 0; i < count; i++)
+		{
+			if (next == printed_nodes[i])
+			{
+				printf("-> [%p] %d\n", (void *)next, next->n);
+				return (count);
+			}
+		}
+		current = next;
+	}
+	return (count);
 }
 
