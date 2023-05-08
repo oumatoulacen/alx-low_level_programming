f1a95575c30666df934eb9efc88d3e94ec1f3e37
oumatoulacen <oumatoulacen@gmail.com>
2023-05-08 20:44:56 +0100

    create a file with open system call

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,16 +16,14 @@
 int create_file(const char *filename, char *text_content)
 {
 	int w;
-	int i = 0;
+	size_t i = 0;
 
 	if (filename == NULL)
 		return (-1);
 	while (text_content[i])
 		i++;
-	if (filename == NULL)
-		return (-1);
 	w = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (w == STDERR_FILENO)
+	if (w == -1)
 		return (-1);
 	if (text_content != NULL)
 	{
