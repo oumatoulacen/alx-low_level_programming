38d063fd1cfbb49025c59c229d7773b49f7df2a3
oumatoulacen <oumatoulacen@gmail.com>
2023-05-08 20:34:16 +0100

    create a file with open system call

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: the name of the file to create
+ * @text_content: a NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure (file can not be created,
+ * file can not be written, write “fails”, etc…)
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int w;
+	int i = 0;
+
+	if (filename == NULL)
+		return (-1);
+	while (text_content[i])
+		i++;
+	if (filename == NULL)
+		return (-1);
+	w = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (w == STDERR_FILENO)
+		return (-1);
+	if (text_content != NULL)
+	{
+		if (write(w, text_content, i) == i)
+		{
+			close(w);
+			return (-1);
+		}
+	}
+	close(w);
+	return (1);
+}
