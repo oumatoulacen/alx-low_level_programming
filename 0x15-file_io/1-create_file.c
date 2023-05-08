bb0067a58c646f3a4680aec119673e28065b35d0
oumatoulacen <oumatoulacen@gmail.com>
2023-05-08 20:37:11 +0100

    create a file with open system call

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,7 +29,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		if (write(w, text_content, i) == i)
+		if (write(w, text_content, i) != i)
 		{
 			close(w);
 			return (-1);
