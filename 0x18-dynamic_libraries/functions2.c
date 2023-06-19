e8250a43c0753fefd57e3c2b8be3bcadfa1dddb3
oumatoulacen <oumatoulacen@gmail.com>
2023-06-19 11:44:24 +0100

    dynamic library

---
+++ 0x18-dynamic_libraries/functions2.c
@@ -0,0 +1,19 @@
+int _isdigit(int c)
+{
+	return (0);
+}
+int _strlen(char *s)
+{
+	return (0);
+}
+void _puts(char *s)
+{
+}
+char *_strcpy(char *dest, char *src)
+{
+	return (0);
+}
+int _atoi(char *s)
+{
+	return (0);
+}
---
+++ 0x18-dynamic_libraries/functions3.c
@@ -0,0 +1,20 @@
+char *_strcat(char *dest, char *src)
+{
+	return (0);
+}
+char *_strncat(char *dest, char *src, int n)
+{
+	return (0);
+}
+char *_strncpy(char *dest, char *src, int n)
+{
+	return (0);
+}
+int _strcmp(char *s1, char *s2)
+{
+	return (0);
+}
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (0);
+}
