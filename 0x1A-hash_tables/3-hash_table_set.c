c46e5209096798a750a914ceb89484f56d601b80
oumatoulacen <oumatoulacen@gmail.com>
2023-07-07 11:47:41 +0100

    add an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -48,7 +48,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(tmp->key, key) == 0)
 		{
-			free(tmp);
+			free(tmp->value);
 			tmp->value = strdup(value);
 			if (!tmp->value)
 				return (0);
