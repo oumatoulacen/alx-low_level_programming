64a41b893ee5b5a1d6da3dbda97da34f9dc040aa
oumatoulacen <oumatoulacen@gmail.com>
2023-07-07 13:29:25 +0100

    print ht

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,24 +9,24 @@ void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *tmp;
 	unsigned long int i;
-	int flag = 0;
+	int flag = 1;
 
 	if (ht == NULL)
 		return;
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[i])
+		for (tmp = ht->array[i]; tmp; tmp = tmp->next)
 		{
-			if (flag == 1)
-				printf(", ");
-			tmp = ht->array[i];
-			while (tmp)
+			if (flag)
 			{
 				printf("'%s': '%s'", tmp->key, tmp->value);
-				tmp = tmp->next;
+				flag = 0;
 			}
-			flag = 1;
+			else
+			{
+				printf(", '%s': '%s'", tmp->key, tmp->value);
+			}
 		}
 	}
 	printf("}\n");
